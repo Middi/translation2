1d6bef2aed85fb7fc28c1b6fd541cda399ad6f91
“Richard Middleton” <Richard@richardmiddleton.me>
2017-11-19 20:17:09 +0000

    added url hash for selecting tabs in url

--- public/js/main-min.js
+++ public/js/main-min.js
@@ -1,1 +1,1 @@
-$("#myTabs a").click(function(a){a.preventDefault(),$(this).tab("show")}),window.location.href.indexOf("norwegian")>-1&&$(".flag").attr("src","/images/norway.png");
+$("#myTabs a").click(function(o){o.preventDefault(),$(this).tab("show")}),window.location.href.indexOf("norwegian")>-1&&$(".flag").attr("src","/images/norway.png"),$(function(){var o=window.location.hash;o&&$('ul.nav a[href="'+o+'"]').tab("show"),$(".nav-tabs a").click(function(o){$(this).tab("show");var a=$("body").scrollTop()||$("html").scrollTop();window.location.hash=this.hash,$("html,body").scrollTop(a)})});
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,20 @@
 $('#myTabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
+  e.preventDefault();
+  $(this).tab('show');
 });
 
 if (window.location.href.indexOf("norwegian") > -1) {
      $('.flag').attr('src', '/images/norway.png');
 }
+
+$(function(){
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+  $('.nav-tabs a').click(function (e) {
+    $(this).tab('show');
+    var scrollmem = $('body').scrollTop() || $('html').scrollTop();
+    window.location.hash = this.hash;
+    $('html,body').scrollTop(scrollmem);
+  });
+});
